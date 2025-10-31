b27098ac084f8bb8e1dea58f077d9890992c014c
harshitvinfotech-pixel <harshit.vinfotech@gmail.com>
2025-10-31 18:07:08 +0530

    Updated IntroSection.tsx

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -42,7 +42,7 @@ export default function IntroSection() {
         </h2>
 
         <p className={`text-xl sm:text-lg md:text-xl lg:text-2xl text-gray-700 dark:text-gray-300 leading-relaxed sm:leading-relaxed md:leading-relaxed transition-all duration-1000 sm:px-6 md:px-2 ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`} style={{ transitionDelay: '200ms' }}>
-         <b>Stop settling for generic SaaS.</b> We engineer high-impact, custom AI solutions that solve your hardest problems. From factual answers with <b>Enterprise RAG</b> to autonomous processes with <b>Computer Vision</b>, we turn your most ambitious ideas into <b>production-ready software—fast.</b>
+         <b>Stop settling for generic SaaS.</b> We engineer high-impact, custom AI solutions that solve your hardest problems. From factual answers with <b>Enterprise RAG</b> to autonomous processes with <b>Computer Vision</b>, we turn your most ambitious ideas into <b>production ready software—fast.</b>
         </p>
       </div>
     </section>
