3e4627f5b472a81cd79121f6336b4de73577cef6
harshitvinfotech-pixel <harshit.vinfotech@gmail.com>
2025-10-31 19:10:30 +0530

    Updated WhyUs.tsx

--- src/components/WhyUs.tsx
+++ src/components/WhyUs.tsx
@@ -72,7 +72,7 @@ export default function WhyUs() {
       bgColor: 'from-[#00B46A]/5 to-[#00B46A]/5',
       borderColor: 'border-[#00B46A]/20',
       hoverBorder: 'hover:border-[#00B46A]/40',
-      image: 'https://images.pexels.com/photos/8728382/pexels-photo-8728382.jpeg?auto=compress&cs=tinysrgb&w=800',
+      image: '/Built Inside.jpeg?auto=compress&cs=tinysrgb&w=800',
     },
   ];
 
