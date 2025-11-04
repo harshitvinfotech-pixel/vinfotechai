c19f2994e7ba1a3551483cc5ed6bc9af062a0a8f
harshitvinfotech-pixel <harshit.vinfotech@gmail.com>
2025-11-04 18:59:25 +0530

    Updated QuoteBannerSection.tsx

--- src/components/case-study-sections/QuoteBannerSection.tsx
+++ src/components/case-study-sections/QuoteBannerSection.tsx
@@ -11,13 +11,11 @@ export default function QuoteBannerSection({ quoteText, quoteAttribution }: Quot
       <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12">
         <div className="flex items-center gap-8 lg:gap-12">
           <div className="flex-shrink-0">
-            <div className="w-20 h-20 sm:w-24 sm:h-24 lg:w-28 lg:h-28 rounded-full bg-white flex items-center justify-center p-4">
-              <img
-                src="/qt.png"
-                alt="Quote"
-                className="w-full h-full object-contain"
-              />
-            </div>
+            <img
+              src="/qt.png"
+              alt="Quote"
+              className="w-20 h-20 sm:w-24 sm:h-24 lg:w-28 lg:h-28 object-contain"
+            />
           </div>
 
           <div className="flex-1">
