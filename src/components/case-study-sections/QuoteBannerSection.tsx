965872d3f1cdadeeefaf01e78439a78f4e2682f6
harshitvinfotech-pixel <harshit.vinfotech@gmail.com>
2025-11-04 17:51:20 +0530

    Updated QuoteBannerSection.tsx

--- src/components/case-study-sections/QuoteBannerSection.tsx
+++ src/components/case-study-sections/QuoteBannerSection.tsx
@@ -14,18 +14,20 @@ export default function QuoteBannerSection({ quoteText, quoteAttribution }: Quot
             <div className="w-20 h-20 sm:w-24 sm:h-24 rounded-full bg-white flex items-center justify-center shadow-lg">
               <svg
                 className="w-10 h-10 sm:w-12 sm:h-12"
-                viewBox="0 0 24 24"
+                viewBox="0 0 48 48"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
                 <path
-                  d="M10 8C10 6.89543 9.10457 6 8 6C6.89543 6 6 6.89543 6 8V10C6 11.1046 6.89543 12 8 12C9.10457 12 10 11.1046 10 10V8Z"
+                  d="M12 18C12 15.7909 13.7909 14 16 14C18.2091 14 20 15.7909 20 18V22C20 24.2091 18.2091 26 16 26C13.7909 26 12 24.2091 12 22V18Z"
                   fill="#00B46A"
                 />
                 <path
-                  d="M18 8C18 6.89543 17.1046 6 16 6C14.8954 6 14 6.89543 14 8V10C14 11.1046 14.8954 12 16 12C17.1046 12 18 11.1046 18 10V8Z"
+                  d="M28 18C28 15.7909 29.7909 14 32 14C34.2091 14 36 15.7909 36 18V22C36 24.2091 34.2091 26 32 26C29.7909 26 28 24.2091 28 22V18Z"
                   fill="#00B46A"
                 />
+                <rect x="12" y="22" width="8" height="8" rx="4" fill="#00B46A"/>
+                <rect x="28" y="22" width="8" height="8" rx="4" fill="#00B46A"/>
               </svg>
             </div>
           </div>
