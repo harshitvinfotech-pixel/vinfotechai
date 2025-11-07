db7554e383d2e042c799a57c2d7c47e3754d8eea
harshitvinfotech-pixel <harshit.vinfotech@gmail.com>
2025-11-07 19:18:41 +0530

    Updated Blogs.tsx

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -89,7 +89,7 @@ export default function Blogs() {
         </div>
       </div>
 
-      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 py-12">
+      <div className="max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 py-8">
         {loading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {[1, 2, 3, 4, 5, 6].map((i) => (
